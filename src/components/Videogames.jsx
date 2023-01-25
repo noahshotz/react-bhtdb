31f718cd2361dbcdef37bc71e6a5f7f741a8575c
noahshotz <max.heinrich@uusimaa.de>
2023-01-26 00:09:53 +0100

    setting headers

--- src/components/Videogames.jsx
+++ src/components/Videogames.jsx
@@ -6,7 +6,15 @@ import { FiLoader as Loading } from "react-icons/fi"
 
 async function getSampleData(isLoading, setLoading, myData, setMyData) {
     const url = "https://web-production-0fb1.up.railway.app/www.backend-bhtdb-production.up.railway.app/videogames"
-    await axios.get(url).then(response => {
+    // set the request headers "origin,x-requested-with" to "https://web-production-0fb1.up.railway.app" and "XMLHttpRequest"
+
+    await axios.get(url, 
+        {
+            headers: {
+                "origin": "https://web-production-0fb1.up.railway.app",
+                "x-requested-with": "XMLHttpRequest"
+            }
+        }).then(response => {
         setMyData(response.data);
         setLoading(false);
 
